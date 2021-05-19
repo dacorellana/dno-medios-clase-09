e2d1710051020cc7147bf1e4ac09a118ed06ab26
dacorellana <54025897+dacorellana@users.noreply.github.com>
2021-05-19 19:34:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function primeraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#FF5757";
+}
+function segundaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#A3E6FF";
+}
+function terceraRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#FFAEAE";
+}
+function cuartaRevista() {
+    document.getElementsByTagName("body")[0].style.background = "#2BC5FF";
+}
+function nada() {
+    document.getElementsByTagName("body")[0].style.background = "#222";
+}
